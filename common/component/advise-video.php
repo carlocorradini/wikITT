8a6f6c17de12942daeb71f5307a1d31ec856b8d5
Paolo Tasin <paolo.tasin17@gmail.com>
2017-03-21 22:31:55 +0100

    - Footer animation - advise videos complete - livesearch with javascript control onblur, onfocus, onkeyup

--- common/component/advise-video.php
+++ common/component/advise-video.php
@@ -44,17 +44,8 @@ function randomGen($min, $max, $quantity) {
         $nVideoDB = 0;
         $nVideo = 8; 
         $risCount = mysqli_query($connetti, "SELECT COUNT(*) AS 'numeroVideo' FROM wikitt.video") or die (mysqli_error($connetti));
-        if(mysqli_num_rows($risCount) > 0){
-            while($row=mysqli_fetch_array($risCount)){
-                $nVideoDB = $row['numeroVideo'];
-            }
-        }
-        if($nVideoDB<8){
-            $nVideo = $nVideoDB;
-        }
-        $risRand = mysqli_query($connetti, "SELECT DISTINCT m.nome as materia, v.titolo as titoloVideo, v.descrizione as descrizioneVideo, a.nome as nomeAutore, a.cognome as cognomeAutore, v.pathMiniatura FROM video v, materia m, autore a, realizza r WHERE v.CodMateria = m.Cod AND a.ID = r.IDAutore AND v.Cod = r.CodVideo ORDER BY RAND() LIMIT 8") or die (mysqli_error($connetti));      
+        $risRand = mysqli_query($connetti, "SELECT DISTINCT m.nome as materia, v.titolo as titoloVideo, v.descrizione as descrizioneVideo, a.nome as nomeAutore, a.cognome as cognomeAutore, v.pathMiniatura FROM video v, materia m, autore a, realizza r WHERE v.CodMateria = m.Cod AND a.ID = r.IDAutore AND v.Cod = r.CodVideo ORDER BY RAND() LIMIT 8 ") or die (mysqli_error($connetti));      
         if(mysqli_num_rows($risRand) > 0){
-            for($i=0;$nVideo>$i;$i++){ 
                 while($row=mysqli_fetch_array($risRand)){       
                 ?>
                     <div class="column">
@@ -62,8 +53,10 @@ function randomGen($min, $max, $quantity) {
 
                             <div class="image">
                                 <div class="ui green ribbon label"><?php echo $row['materia']?></div>
-                            <img src="<?php echo $row['pathMiniatura']?>">
-                        </div>
+                            
+                                <img src="http://scritti9212.altervista.org/scritti9212guide/wp-content/uploads/2013/07/codice-binario.jpg">
+                                <!--<img src="<?php echo $row['pathMiniatura']?>">-->
+                            </div>
                         <div class="content">
                             <div><h2><?php echo $row['titoloVideo']?></h2></div>
                             <div class="description">
@@ -81,7 +74,6 @@ function randomGen($min, $max, $quantity) {
                 <?php
                 }
             }
-        }
         ?>
     
 </div>
--- common/component/livesearch.php
+++ common/component/livesearch.php
@@ -1,4 +1,6 @@
+
 <?php
+
 function connessione(){
     global $connetti;
     $dbHost = "localhost";
@@ -37,21 +39,58 @@ function connessione(){
       xmlhttp.send();
     }
 </script>
+<style>
+    .suggestion li {
+      background:white;
+      list-style: none;
+      padding:7px;
+      transition:background 0.2s;
+      display:flex;
+      justify-content:space-between;
+
+    }
+    li:hover {
+      background:  #f1f1f1;
+    }
+    
+    .suggestion {
+        margin: 0;
+        padding: 0;
+      /*perspective:20px;*/
+    }
+    .suggestion li a{
+        text-decoration: none;
+        color: black;
+        width: 100%;
+        cursor: default;
+        margin-left: 12px;
+
+    }
+
+
+    
+</style>
 <?php 
 connessione();
 // Escape user inputs for security
 $term = mysqli_real_escape_string($connetti, $_REQUEST['q']);
-$ris = mysqli_query($connetti, "SELECT video.titolo FROM wikitt.video WHERE titolo LIKE '$term%' LIMIT 3");
+$ris = mysqli_query($connetti, "SELECT v.titolo as titoloVideo, m.nome nomeMateria, v.link FROM video v,materia m WHERE v.codMateria = m.Cod AND v.titolo LIKE '$term%' LIMIT 3");
 if(mysqli_num_rows($ris) > 0){
-    
+        
+    echo "<ul class='suggestion'>";
         while($row = mysqli_fetch_array($ris)){
-            echo "<p>" . $row['titolo'] . "</p>";
+            //HTML_ENTITY_DECODE()
+            $stringa = ucfirst($row['titoloVideo']);
+            $term = ucfirst(strtolower($term));
+            $completamento = str_replace($term, "", $stringa);
+            echo "<li><a href=".$row['link'].">".$term."<b>".strtolower($completamento)."</b><i style='font-size: 13px;'> - ".$row['nomeMateria']."</i></a></li>";
         }
-   
+    echo "</ul>";
     // Close result set
     mysqli_free_result($ris);
     }else{
-        echo "<p>Nessun risultato trovato</p>";
+        echo "<ul class='suggestion'>";
+            echo "<li><a>Nessun risultato trovato</a></li>";
+        echo "</ul>";
     }
 ?>
-
