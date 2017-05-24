659c3a14d585b481efe4d11eb844a80443039626
carlo.corradini <carlo.corradini@Aula75-pc06.itt.infotelecom>
2017-05-24 08:53:26 +0200

    minor changes

--- admin/video/upload.php
+++ admin/video/upload.php
@@ -10,7 +10,7 @@ session_start();
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Admin | Add Video</title>
+        <title>Admin | Aggiungi Video</title>
         <link rel="icon" href="/common/image/icon.ico" type="image/x-icon">
         
         <!--Frameworks-->
@@ -56,71 +56,84 @@ session_start();
                 });
             });
         </script>
+        <style>
+            #upload-video-panel {
+                width: 1000px;
+                max-width: 100%;
+                margin: 0 auto;
+                padding: 0 10px;
+            }
+        </style>
         <div class="wrapper">
-            <!--#include virtual="/common/component/header.html" -->
-            <?php
+            <?php include $_SERVER["DOCUMENT_ROOT"]."/common/component/header.html";
             if(authentication_session()){
                 require '../admin_navigator.php';
             ?>
-            <div class="main">
-                <div class="ui container">
-                    <h1 class="center_aligned">Aggiunta nuovo video</h1>
-                        <!--
-                        Campi FORM Video:
-                            VideoID
-                            Materia
-                            Autori
-                        Dati impliciti o ricavati:
-                            COD
-                            Titolo
-                            Descrizione
-                            -->
-
-                        <form class="ui form" id="uploadVideo1" method="post" action="/common/php/uploadVid.php">
-                            <div class="field">
-                                <label>YouTube VideoID</label>
-                                <div class="ui left icon input">
-                                    <i class="video play icon"></i>
-                                    <input type="text"  id="user" name="videoID" placeholder="VideoID">
-                                </div>
+                <div id="upload-video-panel">
+                    <!--
+                    Campi FORM Video:
+                        VideoID
+                        Materia
+                        Autori
+                    Dati impliciti o ricavati:
+                        COD
+                        Titolo
+                        Descrizione
+                    -->
+                    <form class="ui form  attached segment" id="uploadVideo1" method="post" action="/common/php/uploadVid.php">
+                        <h1 class="ui horizontal divider header">
+                            <i class="icons">
+                                <i class="video icon"></i>
+                                <i class="corner add icon"></i>
+                            </i>
+                            Nuovo Video
+                        </h1>
+                        <div class="field">
+                            <label>YouTube VideoID</label>
+                            <div class="ui left icon input">
+                                <i class="video play icon"></i>
+                                <input type="text"  id="user" name="videoID" placeholder="VideoID">
                             </div>
+                        </div>
 
-                            <div class="ui fluid search selection dropdown field ">
-                                <input name="materia" type="hidden">
-                                <i class="dropdown icon"></i>
-                                <div class="default text">Materia</div>
-                                <div class="menu">
-                                    <?php
-                                        $ris = query("SELECT DISTINCT m.Cod, m.Nome FROM materia m, indirizzo i ORDER BY i.Nome;");
-                                        while ($row= mysqli_fetch_array($ris)){
-                                            echo "<div class='item' data-value=$row[Cod]>$row[Nome] ($row[Cod])</div>";
-                                        }
-                                    ?>
-                                </div>
+                        <div class="ui fluid search selection dropdown field ">
+                            <input name="materia" type="hidden">
+                            <i class="dropdown icon"></i>
+                            <div class="default text">Materia</div>
+                            <div class="menu">
+                                <?php
+                                    $ris = query("SELECT DISTINCT m.Cod, m.Nome FROM materia m, indirizzo i ORDER BY i.Nome;");
+                                    while ($row= mysqli_fetch_array($ris)){
+                                        echo "<div class='item' data-value=$row[Cod]>$row[Nome] ($row[Cod])</div>";
+                                    }
+                                ?>
                             </div>
+                        </div>
 
-                            <div class="ui fluid multiple search selection dropdown field">
-                                <input name="autori" type="hidden">
-                                <i class="dropdown icon"></i>
-                                <div class="default text">Autori</div>
-                                <div class="menu">
-                                    <?php
-                                        $ris = query("SELECT a.ID, a.Nome, a.Cognome, a.Classe FROM autore a ORDER BY a.Classe, a.Cognome, a.Nome;");
-                                        while ($row= mysqli_fetch_array($ris)){
-                                            echo "<div class='item' data-value=$row[ID]>$row[Cognome] $row[Nome] ($row[Classe])</div>";
-                                        }
-                                    ?>
-                                </div>
+                        <div class="ui fluid multiple search selection dropdown field">
+                            <input name="autori" type="hidden">
+                            <i class="dropdown icon"></i>
+                            <div class="default text">Autori</div>
+                            <div class="menu">
+                                <?php
+                                    $ris = query("SELECT a.ID, a.Nome, a.Cognome, a.Classe FROM autore a ORDER BY a.Classe, a.Cognome, a.Nome;");
+                                    while ($row= mysqli_fetch_array($ris)){
+                                        echo "<div class='item' data-value=$row[ID]>$row[Cognome] $row[Nome] ($row[Classe])</div>";
+                                    }
+                                ?>
                             </div>
-                            <button type="submit" class="ui button blue">Aggiungi</button>  
-                        </form>  
+                        </div>
+                        <button type="submit" class="ui button blue">Aggiungi</button>  
+                    </form>
+                    <div id="form-video-msg" class="ui bottom attached message">
+                        <i class="send icon"></i>
+                        <span>Inserire dati video</span>
                     </div>
-                </div> 
+                </div>
             <?php } else {
                 header("Location: /admin/index.php");
                 die();
             }?>
         </div>
-        <!--#include virtual="/common/component/footer.html" -->
     </body>
 </html>
