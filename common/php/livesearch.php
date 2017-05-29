820443c0bf66bd1f52955fed064f3850af0f9a8a
paolo.tasin <paolo.tasin@Aula74-pc14.itt.infotelecom>
2017-05-29 12:47:12 +0200

    Risolto

--- common/php/livesearch.php
+++ common/php/livesearch.php
@@ -7,15 +7,10 @@
     $data = array();
     // Escape user inputs for security
     $term = mysqli_real_escape_string($connection, filter_input(INPUT_GET, "q"));
-    echo $term;
-    $result = query("SELECT v.titolo as titoloVideo, m.nome as nomeMateria, v.VideoID as vId, LOCATE('$term', v.titolo) as score FROM video v,materia m WHERE v.CodMateria = m.Cod AND v.titolo LIKE '%$term%' ORDER BY score LIMIT 8");
+    $result = query("SELECT v.titolo as titoloVideo, m.nome as nomeMateria, v.VideoID as vId, LOCATE('$term', v.titolo) as score FROM video v,materia m WHERE v.CodMateria = m.Cod AND v.titolo LIKE '%$term%' ORDER BY score LIMIT 3");
     
     if(mysqli_num_rows($result) > 0) {
         while ($row = mysqli_fetch_array($result)) {
-            echo $row['vId']."<br>";
-            echo $row['titoloVideo']."<br>";
-            echo $row['nomeMateria']."<br>";
-            echo "";
             $suggestion = array(
                 "vId" => $row["vId"],
                 "titolo" => $row["titoloVideo"],
